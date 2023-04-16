181e6f7a39a6825552729eda5c2a71e9100daaa0
Nabanita <nroy0110@gmail.com>
2023-04-16 22:54:38 +0100

    Initial Commit

---
+++ webapp.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# Audience - Recruiters, General
+# Type of platform - CV, LinkedIn
+# Focus on - Extract from CV
+# Job Description  - Match for CV option
+# Number of words
+# Set Tone - Default - Professional
+
