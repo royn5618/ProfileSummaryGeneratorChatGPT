9392e215977cafed312347ef7a259d7d888552d9
Nabanita <nroy0110@gmail.com>
2023-05-25 05:52:48 +0100

    Adding final changes

--- openai_helper.py
+++ openai_helper.py
@@ -1,2 +1,17 @@
-def get_response():
-    pass
+import openai
+import config
+
+openai.api_key = config.OPENAI_API_KEY
+
+
+# role - ['system', 'assistant', 'user']
+def get_response(prompt, role='user'):
+    # TODO Add temperature as an argument
+    generated_text = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": role, "content": prompt}
+        ],
+        temperature=0.5
+    )
+    return generated_text['choices'][0]['message']['content']
--- webapp.py
+++ webapp.py
@@ -1,7 +1,9 @@
 import streamlit as st
-from  helper import *
+from helper import *
+import openai
+from openai_helper import get_response
 
-st.set_page_config(layout="wide",page_title="CV Assistant")
+st.set_page_config(layout="wide", page_title="CV Assistant")
 st.header(":orange[PyCon Demo: Resume Building Assistant]")
 st.markdown("""---""")
 
@@ -11,44 +13,37 @@ with st.form("form1"):
     if uploaded_file is not None:
         try:
             profile = extract_data(uploaded_file)
-            st.write(profile)
+            # st.write(profile)
         except:
             st.error('Unable to parse the uploaded file', icon="😞")
     gen_sum = st.form_submit_button("Generate Summary")
     if gen_sum:
-        prompt_builder = "Generate a summary of the profile."
-        """
-        BASIC Additions
-        1. Add word limit - Using max_tokens or specifying in the prompt.
-        2. Set a Tone - Add to the prompt - "Professional"
-        """
-        st.write("Summary Generated")
+        # TODO: Add a tone.
+        tone = None
+        # TODO: Add target audience
+        target = None
+        # TODO: Add first/third person
+        person = None
+        prompt_builder = f"Generate a summary of the profile: --- {profile} --- "
+        _summary = get_response(prompt=prompt_builder)
+        st.write(_summary)
 
 # PART 2
 with st.form("form2"):
     job_desc = st.text_area(":orange[Enter a Job Description:]")
     ext_kwds = st.form_submit_button("Extract Keywords")
     if ext_kwds:
+        prompt_builder = f"Extract keywords from this job description: {job_desc}"
+        _keywords = get_response(prompt=prompt_builder)
         st.write("Extracted Keywords")
+        st.write(_keywords)
 
 # PART 3
 with st.form("form3"):
     # COMBINE BOTH: Generate a summary using the keywords.
-    job_desc = st.text_area(":orange[Enter a Job Description:]")
-    profile = None
-    uploaded_file = st.file_uploader(":orange[Upload Your CV to Generate Profile Summary:]")
-    if uploaded_file is not None:
-        try:
-            profile = extract_data(uploaded_file)
-        except:
-            st.error('Unable to parse the uploaded file', icon="😞")
-    gen_sum_again = st.form_submit_button("Generate Summary")
-    keywords = "Get Keywords"  # TODO
-    if gen_sum_again:
-        prompt_builder = f"Generate a summary of the profile using the following keywords: {keywords}"
-        """
-        BASIC Additions
-        1. Add word limit - Using max_tokens or specifying in the prompt.
-        2. Set a Tone - Add to the prompt - "Professional"
-        """
-        st.write("Summary Generated")
+    given_content = st.text_area(":orange[Rewrite the following content:]")
+    button_rewrite = st.form_submit_button("Rewrite")
+    if button_rewrite:
+        prompt_builder = f"Rewrite the following in a professional tone in five bullets: {given_content}"
+        rewritten_content = get_response(prompt=prompt_builder)
+        st.write(rewritten_content)
